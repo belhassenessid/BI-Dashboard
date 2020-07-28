b4bfd5622d37edc939c351540e42baa786f0f3e0
belhassenessid <belhassen.essid@etudiant-isi.utm.tn>
2020-07-29 00:08:45 +0100

    close hide

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ st.sidebar.markdown("### Number of tweets by sentiment")
 select = st.sidebar.selectbox('Visualization type', ['Bar plot', 'Pie chart'], key='1')
 sentiment_count = data['airline_sentiment'].value_counts()
 sentiment_count = pd.DataFrame({'Sentiment':sentiment_count.index, 'Tweets':sentiment_count.values})
-if not st.sidebar.checkbox("Hide", True):
+if not st.sidebar.checkbox("Hide", False):
     st.markdown("### Number of tweets by sentiment")
     if select == 'Bar plot':
         fig = px.bar(sentiment_count, x='Sentiment', y='Tweets', color='Tweets', height=500)
@@ -44,7 +44,7 @@ if not st.sidebar.checkbox("Hide", True):
 st.sidebar.subheader("When and where are users tweeting from?")
 hour = st.sidebar.slider("Hour to look at", 0, 23)
 modified_data = data[data['tweet_created'].dt.hour == hour]
-if not st.sidebar.checkbox("Close", True, key='1'):
+if not st.sidebar.checkbox("Close", False, key='1'):
     st.markdown("### Tweet locations based on time of day")
     st.markdown("%i tweets between %i:00 and %i:00" % (len(modified_data), hour, (hour + 1) % 24))
     st.map(modified_data)
@@ -56,7 +56,7 @@ st.sidebar.subheader("Total number of tweets for each airline")
 each_airline = st.sidebar.selectbox('Visualization type', ['Bar plot', 'Pie chart'], key='2')
 airline_sentiment_count = data.groupby('airline')['airline_sentiment'].count().sort_values(ascending=False)
 airline_sentiment_count = pd.DataFrame({'Airline':airline_sentiment_count.index, 'Tweets':airline_sentiment_count.values.flatten()})
-if not st.sidebar.checkbox("Close", True, key='2'):
+if not st.sidebar.checkbox("Close", False, key='2'):
     if each_airline == 'Bar plot':
         st.subheader("Total number of tweets for each airline")
         fig_1 = px.bar(airline_sentiment_count, x='Airline', y='Tweets', color='Tweets', height=500)
@@ -113,7 +113,7 @@ if len(choice) > 0:
 
 st.sidebar.header("Word Cloud")
 word_sentiment = st.sidebar.radio('Display word cloud for what sentiment?', ('positive', 'neutral', 'negative'))
-if not st.sidebar.checkbox("Close", True, key='3'):
+if not st.sidebar.checkbox("Close", False, key='3'):
     st.subheader('Word cloud for %s sentiment' % (word_sentiment))
     df = data[data['airline_sentiment']==word_sentiment]
     words = ' '.join(df['text'])
